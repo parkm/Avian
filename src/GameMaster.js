9df644d9b5b3feed069e5eec4dbb1f4f8fe4d012
Parker Miller <parkermiller00@gmail.com>
2018-03-22 20:27:09 -0600

    Add more to bird

--- src/GameMaster.js
+++ src/GameMaster.js
@@ -9,25 +9,25 @@ export default class GameMaster {
     this.money = 0;
 
     this.ownedBirds = [
-      new Bird('PlayerBird', new BirdStats({
+      new Bird('PlayerBird', 'male', 'average', new BirdStats({
         topMph: 30,
         accel: 5,
         stamina: 5,
         vigor: 10
       })),
-      new Bird('PlayerBird2', new BirdStats({
+      new Bird('PlayerBird2', 'female', 'average', new BirdStats({
         topMph: 200,
         accel: 10,
         stamina: 10,
         vigor: 10
       })),
-      new Bird('SuperBird', new BirdStats({
+      new Bird('SuperBird', 'male', 'wonderful', new BirdStats({
         topMph: 1000,
         accel: 30,
         stamina: 30,
         vigor: 30
       })),
-      new Bird('The Winner', new BirdStats({
+      new Bird('The Winner', 'female', 'perfect', new BirdStats({
         topMph: 5000,
         accel: 5000,
         stamina: 30,
--- src/game/Bird.js
+++ src/game/Bird.js
@@ -1,6 +1,10 @@
 export default class Bird {
-  constructor(name, stats) {
+  static GENES = ['poor', 'average', 'good', 'great', 'wonderful', 'perfect'];
+
+  constructor(name, sex, genes, stats) {
     this.name = name;
+    this.sex = sex;
+    this.genes = genes;
     this.stats = stats;
   }
 }
